5167bddd7f37459ed244c776e560e4e3e9be0615
Mahmudul <mhasan0047@gmail.com>
2018-12-24 04:28:28 -0500

    finished 3

--- HW3_Doubly_Linked_List/dnode.h
+++ HW3_Doubly_Linked_List/dnode.h
@@ -1,36 +1,41 @@
 //The node class for a doubly linked list typically looks something like this:
-//#ifndef MAIN_SAVITCH_DNODE_H  
-//#define MAIN_SAVITCH_DNODE_H
 #include <cassert>    // Provides assert
 #include <cstdlib>    // Provides NULL and size_t
 
 class dnode {
     public: 
         typedef double value_type;
-        dnode( const value_type& init_data = value_type(), dnode* init_next = NULL, dnode* init_previous = NULL )
-        {
-            data_field = init_data; 
-            next_link = init_next; 
-            prev_link = init_previous;
-        }
+        dnode( const value_type& init_data = value_type(), dnode* init_next = NULL, dnode* init_prev = NULL );
         // Member functions to set the data and link fields:
         void set_data(const value_type& new_data) { data_field = new_data; }
         void set_next(dnode* new_next) { next_link = new_next; }
-        void set_previous(dnode* new_previous) { prev_link = new_previous; }
+        void set_prev(dnode* new_prev) { prev_link = new_prev; }
         // Const member function to retrieve the current data:
         value_type data() const { return data_field; }
         // Two slightly different member functions to retrieve each current link:
         const dnode* next() const { return next_link; }
         dnode* next() { return next_link; }
-        const dnode* previous() const { return prev_link; }
-        dnode* previous() { return prev_link; }
+        const dnode* prev() const { return prev_link; }
+        dnode* prev() { return prev_link; }
+
+        std::size_t length( const dnode* head_ptr);
+        void head_insert(dnode*& head_ptr, const dnode::value_type& entry);
+        void insert(dnode* prev_ptr, const dnode::value_type& entry);
+        dnode* search(dnode* head_ptr, const dnode::value_type& target);
+        const dnode* search(const dnode* head_ptr, const dnode::value_type& target); dnode* locate(dnode* head_ptr, std::size_t position);
+        const dnode* locate(const dnode* head_ptr, std::size_t position);
+        void list_head_remove(dnode*& head_ptr);
+        void list_remove(dnode* prev_ptr);
+        void list_clear(dnode*& head_ptr);
+        void list_copy(const dnode* source_ptr, dnode*& head_ptr, dnode*& tail_ptr);
+
     private:
         value_type data_field;
         dnode *next_link;
         dnode *prev_link;
 
 };
-//#endif
+
 //prev link points to the prev node. next pointer points to next node. 
 
 
--- HW3_Doubly_Linked_List/toolkit.h
+++ HW3_Doubly_Linked_List/toolkit.h
@@ -1,21 +1,20 @@
 
 //Implement the Doubly Linked List as a class with the following methods from the linked list toolkit:
 
-std::size_t length( const node* head_ptr);
-void head_insert(node*& head_ptr, const node::value_type& entry);
-void insert(node* previous_ptr, const node::value_type& entry);
-node* search(node* head_ptr, const node::value_type& target);
-const node* search(const node* head_ptr, const node::value_type& target); node* locate(node* head_ptr, std::size_t position);
-const node* locate(const node* head_ptr, std::size_t position);
-void list_head_remove(node*& head_ptr);
-void list_remove(node* previous_ptr);
-void list_clear(node*& head_ptr);
-void list_copy(const node* source_ptr, node*& head_ptr, node*& tail_ptr);
-
+       std::size_t length( const dnode* head_ptr);
+        void head_insert(dnode*& head_ptr, const dnode::value_type& entry);
+        void insert(dnode* previous_ptr, const dnode::value_type& entry);
+        dnode* search(dnode* head_ptr, const dnode::value_type& target);
+        const dnode* search(const dnode* head_ptr, const dnode::value_type& target); dnode* locate(dnode* head_ptr, std::size_t position);
+        const dnode* locate(const dnode* head_ptr, std::size_t position);
+        void list_head_remove(dnode*& head_ptr);
+        void list_remove(dnode* previous_ptr);
+        void list_clear(dnode*& head_ptr);
+        void list_copy(const dnode* source_ptr, dnode*& head_ptr, dnode*& tail_ptr);
 
 
 //not in toolkit: implement these methods that are not in the toolkit:
 value_type operator[](std::size_t position)//same as locate, return data at position
-bool has_cycle(node*& head_ptr) //return true if there's a cycle
-node* find_cycle(node*& head_ptr)//return node in cycle
+bool has_cycle(dnode*& head_ptr) //return true if there's a cycle
+dnode* find_cycle(dnode*& head_ptr)//return dnode in cycle
 //Find more information on finding cycles at: https://blog.ostermiller.org/find-loop-singly-linked-list
