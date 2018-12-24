e34847bcc4f22815114a62d37370f50df44d5b4d
Mahmudul <mhasan0047@gmail.com>
2018-12-24 03:30:18 -0500

    fixed some stuff

--- HW3_Doubly_Linked_List/dnode.h
+++ HW3_Doubly_Linked_List/dnode.h
@@ -1,30 +1,36 @@
 //The node class for a doubly linked list typically looks something like this:
+#ifndef MAIN_SAVITCH_DNODE_H  
+#define MAIN_SAVITCH_DNODE_H
+#include <cassert>    // Provides assert
+#include <cstdlib>    // Provides NULL and size_t
+
 class dnode {
-public: // TYPEDEF
-    typedef double value_type;
-    // CONSTRUCTOR
-    dnode( const value_type& init_data = value_type(), dnode* init_next = NULL, dnode* init_previous = NULL )
-    {
-        data_field = init_data; 
-        next = init_next; 
-        previous = init_previous;
-    }
-    // Member functions to set the data and link fields:
-    void set_data(const value_type& new_data) { data_field = new_data; }
-    void set_next(dnode* new_next) { next = new_next; }
-    void set_previous(dnode* new_previous) { previous = new_previous; }
-    // Const member function to retrieve the current data:
-    value_type data() const { return data_field; }
-    // Two slightly different member functions to retrieve each current link:
-    const dnode* next() const { return next; }
-    dnode* next() { return next; }
-    const dnode* previous() const { return previous; }
-    dnode* previous() { return previous; }
-private:
-    value_type data_field;
-    dnode *next;
-    dnode *previous;
+    public: 
+        typedef double value_type;
+        dnode( const value_type& init_data = value_type(), dnode* init_next = NULL, dnode* init_previous = NULL )
+        {
+            data_field = init_data; 
+            next_link = init_next; 
+            prev_link = init_previous;
+        }
+        // Member functions to set the data and link fields:
+        void set_data(const value_type& new_data) { data_field = new_data; }
+        void set_next(dnode* new_next) { next_link = new_next; }
+        void set_previous(dnode* new_previous) { prev_link = new_previous; }
+        // Const member function to retrieve the current data:
+        value_type data() const { return data_field; }
+        // Two slightly different member functions to retrieve each current link:
+        const dnode* next() const { return next_link; }
+        dnode* next() { return next_link; }
+        const dnode* previous() const { return prev_link; }
+        dnode* previous() { return prev_link; }
+    private:
+        value_type data_field;
+        dnode *next_link;
+        dnode *prev_link;
+
 };
+#endif
 //prev link points to the prev node. next pointer points to next node. 
 
 
