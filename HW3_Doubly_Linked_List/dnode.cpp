ddc18e844be8b2eb26d24bc12c9150b045eb1ac2
Mahmudul <mhasan0047@gmail.com>
2018-12-24 19:41:21 -0500

    moves them out of class

--- HW3_Doubly_Linked_List/dnode.cpp
+++ HW3_Doubly_Linked_List/dnode.cpp
@@ -7,7 +7,7 @@ dnode::dnode( const value_type& init_data = value_type(), dnode* init_next = NUL
     next_link = init_next; 
     prev_link = init_prev;
 }
-size_t dnode::length( const dnode* head_ptr)
+size_t length( const dnode* head_ptr)
 {
     const dnode *cursor;
     size_t answer = 0;
@@ -15,21 +15,21 @@ size_t dnode::length( const dnode* head_ptr)
         ++answer;
     return answer;
 }
-void dnode::head_insert(dnode*& head_ptr, const dnode::value_type& entry)
+void head_insert(dnode*& head_ptr, const dnode::value_type& entry)
 { 
     dnode *new_ptr;
     new_ptr = new dnode(entry, head_ptr, NULL); //1) making new node, back points to NULL (since its new head) and next points to current nex tnode
     head_ptr -> set_prev(new_ptr); //2) linking the old head to the new head
     head_ptr = new_ptr; //3) setting the head_ptr
 }
-void dnode::insert(dnode* prev_ptr, const dnode::value_type& entry)
+void insert(dnode* prev_ptr, const dnode::value_type& entry)
 {
     dnode *insert_ptr;
     insert_ptr = new dnode(entry, prev_ptr->next(), prev_ptr ); //1) new node linked to back and front nodes
     prev_ptr->next()->set_prev(insert_ptr); //2) linking front node to new node
     prev_ptr->set_next(insert_ptr);//3) linking back node to new node
 }
-dnode* dnode::search(dnode* head_ptr, const dnode::value_type& target)
+dnode* search(dnode* head_ptr, const dnode::value_type& target)
 {
 	dnode *cursor;
 	for (cursor = head_ptr; cursor->next() != head_ptr; cursor = cursor->next())
@@ -37,7 +37,7 @@ dnode* dnode::search(dnode* head_ptr, const dnode::value_type& target)
 		    return cursor;
 	return NULL;
 }
-const dnode* dnode::search(const dnode* head_ptr, const dnode::value_type& target)
+const dnode* search(const dnode* head_ptr, const dnode::value_type& target)
 {
 	const dnode *cursor;
 	for (cursor = head_ptr; cursor->next() != head_ptr; cursor = cursor->next())
@@ -45,7 +45,7 @@ const dnode* dnode::search(const dnode* head_ptr, const dnode::value_type& targe
 		    return cursor;
 	return NULL;
 }
-dnode* dnode::locate(dnode* head_ptr, std::size_t position)
+dnode* locate(dnode* head_ptr, std::size_t position)
 {
     dnode *cursor;
     size_t i;
@@ -55,7 +55,7 @@ dnode* dnode::locate(dnode* head_ptr, std::size_t position)
         cursor = cursor->next();
     return cursor;
 }
-const dnode* dnode::locate(const dnode* head_ptr, std::size_t position)
+const dnode* locate(const dnode* head_ptr, std::size_t position)
 {
     const dnode *cursor;
     size_t i;
@@ -65,7 +65,7 @@ const dnode* dnode::locate(const dnode* head_ptr, std::size_t position)
         cursor = cursor->next();
     return cursor;
 }
-void dnode::list_head_remove(dnode*& head_ptr){
+void list_head_remove(dnode*& head_ptr){
     dnode *remove_ptr;
     remove_ptr = head_ptr; //1) mark the head node for deletion
     head_ptr = head_ptr->next(); //2) move the head_ptr to the new head node
@@ -73,17 +73,36 @@ void dnode::list_head_remove(dnode*& head_ptr){
     delete remove_ptr; //4) deleted the old head dnode
 }
 
-void dnode::list_remove(dnode* prev_ptr){
+void list_remove(dnode* prev_ptr){
     dnode *remove_ptr;
     remove_ptr = prev_ptr->next(); //1) marking node for deletion
     remove_ptr->next()->set_prev(prev_ptr);//2) linking front node to back node
     prev_ptr->set_next( remove_ptr->next() );//3) linking back node to front node
     delete remove_ptr; //4) deleting the node
 }
-void dnode::list_clear(dnode*& head_ptr){
+void list_clear(dnode*& head_ptr){
 	while (head_ptr != NULL)	
 	    list_head_remove(head_ptr);
 }
+
+
+
 void list_copy(const dnode* source_ptr, dnode*& head_ptr, dnode*& tail_ptr){//finish
-	
+	//A) SET HEAD AND TAIL POINTERS TO null
+    head_ptr = NULL;
+    tail_ptr = NULL;
+    //B) SPACEIAL CASE: When List Empty.
+    if (source_ptr == NULL)
+        return; 
+    //C) Make the head node for the newly created list, and put data in it.
+    head_insert(head_ptr, source_ptr->data());
+    tail_ptr = head_ptr;
+    //D) Copy the rest of the nodes one at a time, adding at the tail of new list.
+    source_ptr = source_ptr->next(); //(1) TRANSVERSING THRU ORIG LIST: addr of source_ptr = addr of next node
+    while (source_ptr != NULL) //(1) TRANSVERSING THRU ORIG LIST:
+    {
+        insert(tail_ptr, source_ptr->data()); //(2) list_insert(ptr to node that we will insert after, value in the new node)
+        tail_ptr = tail_ptr->next(); //(3) addr of tail_ptr = addr tail_ptr points to
+        source_ptr = source_ptr->next(); //(4) addr of source_ptr = addr of next node
+    } 
 }
--- HW3_Doubly_Linked_List/dnode.h
+++ HW3_Doubly_Linked_List/dnode.h
@@ -17,31 +17,31 @@ class dnode{
         dnode* next() { return next_link; }
         const dnode* prev() const { return prev_link; }
         dnode* prev() { return prev_link; }
-
-        std::size_t length( const dnode* head_ptr);
-        void head_insert(dnode*& head_ptr, const dnode::value_type& entry);
-        void insert(dnode* prev_ptr, const dnode::value_type& entry);
-        dnode* search(dnode* head_ptr, const dnode::value_type& target);
-        const dnode* search(const dnode* head_ptr, const dnode::value_type& target); dnode* locate(dnode* head_ptr, std::size_t position);
-        const dnode* locate(const dnode* head_ptr, std::size_t position);
-        void list_head_remove(dnode*& head_ptr);
-        void list_remove(dnode* prev_ptr);
-        void list_clear(dnode*& head_ptr);
-        void list_copy(const dnode* source_ptr, dnode*& head_ptr, dnode*& tail_ptr);
-
-        //not in toolkit: implement these methods that are not in the toolkit:
-        value_type operator[](std::size_t position)//same as locate, return data at position
-        bool has_cycle(dnode*& head_ptr) //return true if there's a cycle
-        dnode* find_cycle(dnode*& head_ptr)//return dnode in cycle
-        //Find more information on finding cycles at: https://blog.ostermiller.org/find-loop-singly-linked-list
     private:
         value_type data_field;
         dnode *next_link;
         dnode *prev_link;
 
 };
+std::size_t length( const dnode* head_ptr);
+void head_insert(dnode*& head_ptr, const dnode::value_type& entry);
+void insert(dnode* prev_ptr, const dnode::value_type& entry);
+dnode* search(dnode* head_ptr, const dnode::value_type& target);
+const dnode* search(const dnode* head_ptr, const dnode::value_type& target); dnode* locate(dnode* head_ptr, std::size_t position);
+const dnode* locate(const dnode* head_ptr, std::size_t position);
+void list_head_remove(dnode*& head_ptr);
+void list_remove(dnode* prev_ptr);
+void list_clear(dnode*& head_ptr);
+void list_copy(const dnode* source_ptr, dnode*& head_ptr, dnode*& tail_ptr);
 
-//prev link points to the prev node. next pointer points to next node. 
+// //not in toolkit: implement these methods that are not in the toolkit:
+// value_type operator[](std::size_t position)//same as locate, return data at position
+// bool has_cycle(dnode*& head_ptr) //return true if there's a cycle
+// dnode* find_cycle(dnode*& head_ptr)//return dnode in cycle
+// //Find more information on finding cycles at: https://blog.ostermiller.org/find-loop-singly-linked-list
+
+
+// //prev link points to the prev node. next pointer points to next node. 
 
 
 
