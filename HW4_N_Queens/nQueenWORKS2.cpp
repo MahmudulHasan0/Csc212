38d9e346ee364d7ba6fc4f890f63fa1b7f1cd073
Mahmudul <mhasan0047@gmail.com>
2018-12-27 19:56:20 -0500

    WORKING AGAIN

--- HW4_N_Queens/nQueenWORKS2.cpp
+++ HW4_N_Queens/nQueenWORKS2.cpp
@@ -25,6 +25,19 @@ void print(stack<int> s, const int QUEEN)
         cout<<endl;
     }
 }
+void push_Queen(stack <int>& q, bool *&usedCol, int& N, int& prev, int& curr){
+    q.push(curr);         //1) Push the queen
+    --N;                  //2) Got one less queen to put n the board
+    usedCol[curr] = true; //3) Mark the column where u placed the queen so that no other queen can be placed here
+    prev = curr;          //4) Mark the position of the last queen, so that the next queen wouldnt be close to it.
+    curr = 1;             //5) reset the next queens position to 1 so that we can go go through all the positions of the row. 
+}
+void setup_usedCol(bool *&usedCol, int const &QUEEN){
+    for (int i = 0; i<=QUEEN+2; ++i)
+        usedCol[i] = false;
+    usedCol[0]=true;       //Cant put queen in this column bec im considering this col as a "no zone"
+    usedCol[QUEEN+1]=true; //Cant put queen in this column bec im considering this col as a "no zone"
+}
 
 
 int main()
@@ -40,33 +53,22 @@ int main()
     int prev = 1;           //the column position where the queen was just placed in the board
 
     //B) SETTING COLUMN ARRAY TO AVOID PUTTING 2 QUEENS IN SAME COLUMN
-    bool usedCol [QUEEN+2]; //ex: QUEEN=5: 0,1,2,3,4,5,6. 0 and 6 are outofzone columns
-    for (int i = 0; i<=QUEEN+2; i++)
-        usedCol[i] = false;
-    usedCol[0]=true; //cant put queen in this column bec im considering this col as a "no zone"
-    usedCol[QUEEN+1]=true; //cant put queen in this column bec im considering this col as a "no zone"
-
+    bool *usedCol;
+    usedCol = new bool[QUEEN+2]; //ex: QUEEN=5: 0,1,2,3,4,5,6. 0 and 6 are outofzone columns
+    setup_usedCol(usedCol, QUEEN);
     //C) PUT THE FIRST QUEEN IN THE FIRST COLUMN OF THE FIRST ROW
-    queenPos.push(curr);
-    usedCol[curr] = true;
-    prev = curr;
-    curr = 1;
-    N--;
-    int counter = 0; //[Made For Testing Purposes] How many times did we increment the curr position? Did we finish the row?
+    push_Queen(queenPos, usedCol, N, prev, curr);
 
     //C) ALGORITHM TO PUT QUEENS IN THE RIGHT PLACE
     while(N>0)  //Already did one queen. N queens left. stop when N=0 because all queens are in board
     { 
         //DOING SOME BOARD WARPINGS:
-        if (curr > QUEEN) //WARPING: if curr queen's position is QUEEN+1, bring back the pos to 1 
-            curr = 1; 
-        if (curr == 0)   //WARPING: if the curr pos is in the 0 zone, bring it to 1.
+        if (curr > QUEEN || curr == 0) //WARPING: if curr queen's position is in the forbidden zones:  QUEEN+1 or 0 , bring back the pos to 1 
             curr = 1; 
         //D) IF THE CURRENT POS VIOLATE RULES, SHIFT IT BY 1
         if (usedCol[curr] || prev==curr || prev==curr+1 || prev==curr-1) 
         {
             curr++;
-            counter++;
             //A*) REACHED THE END OF THE ROAD. IF U CANT DO ANYTHING, POP THE LAST QUEEN AND SHIFT IT AND REDO 
             if (curr > QUEEN) 
             { 
@@ -83,12 +85,8 @@ int main()
                     if (!usedCol[poppedQueen] && (poppedQueen!=0) && (poppedQueen!=QUEEN+1) && (queenPos.top()!=poppedQueen) && queenPos.top()!=poppedQueen+1 && queenPos.top()!=poppedQueen-1 )
                     {  
                         //5) now push the new position:
-                        queenPos.push(poppedQueen);  
-                        N--;                 
-                        usedCol[poppedQueen] = true; 
-                        prev = poppedQueen;       
-                        curr = 1;             
-                        counter = 0;
+                        curr = poppedQueen; //to use the push_Queen function
+                        push_Queen(queenPos, usedCol, N, prev, curr);
                         needToMove = false;
                     }
                 }
@@ -96,14 +94,7 @@ int main()
         }
         //E) IF NO VIOLATION, PUT THE QUEEN ON THE BOARD
         else
-        {
-            queenPos.push(curr);  //1) Push the queen
-            N--;                  //2) Got one less queen to put n the board
-            usedCol[curr] = true; //3) Mark the column where u placed the queen so that no other queen can be placed here
-            prev = curr;          //4) Mark the position of the last queen, so that the next queen wouldnt be close to it.
-            curr = 1;             //5) reset the next queens position to 1 so that we can go go through all the positions of the row. 
-            counter = 0;
-        }  
+            push_Queen(queenPos, usedCol, N, prev, curr);
     }
     print(queenPos, QUEEN);
     return 0;
